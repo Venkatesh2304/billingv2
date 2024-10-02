476daa0d0ea88da97895cf5a75d22ffb73dbab69
Venkatesh2304 <96430022+Venkatesh2304@users.noreply.github.com>
2024-10-03 01:10:00 +0530

    push command

--- app/management/commands/push.py
+++ app/management/commands/push.py
@@ -4,6 +4,10 @@ import sys
 os.system("rm -rf custom/")
 os.system(f"cp -r /home/venkatesh/.local/lib/python3.10/site-packages/custom custom/")
 os.system(f"git add *")
+os.system(f"git status")
+if input("check git status : ") != "y" : 
+    os.system("rm -rf custom/")
+    exit(0)
 os.system(f"git commit -m '{sys.argv[2]}'")
 os.system("git push")
 
