ed3e7bd1983bfe559f8ccd07785b5898caa07473
Venkatesh2304 <venkateshks2304@gmail.com>
2024-10-28 14:12:46 +0530

    s

--- app/admin.py
+++ app/admin.py
@@ -1421,14 +1421,16 @@ class SalesmanPendingSheetAdmin(CustomAdminModel) :
             if len(reader.pages) % 2 != 0:
                 writer.add_blank_page() 
 
-        combined_pdf = BytesIO()
-        writer.write(combined_pdf)
-        with open("pending_sheet.pdf","wb+") as f : f.write(combined_pdf.getvalue())
-        combined_pdf.seek(0)
-        messages.success(request,mark_safe(f"Download Pending Sheet : {hyperlink('/static/pending_sheet.pdf','PENDING_SHEET')}"))
-        response = HttpResponse(combined_pdf.getvalue(), content_type='application/pdf')
-        response['Content-Disposition'] = 'attachment; filename="' + f"pending_sheet.pdf" + '"'
-        return response 
+        writer.write("pending_sheet.pdf")
+        writer.close()
+
+        # combined_pdf = BytesIO()
+        # with open("pending_sheet.pdf","wb+") as f : f.write(combined_pdf.getvalue())
+        # combined_pdf.seek(0)
+        # messages.success(request,mark_safe(f"Download Pending Sheet : {hyperlink('/static/pending_sheet.pdf','PENDING_SHEET')}"))
+        # response = HttpResponse(combined_pdf.getvalue(), content_type='application/pdf')
+        # response['Content-Disposition'] = 'attachment; filename="' + f"pending_sheet.pdf" + '"'
+        # return response 
         
     def changelist_view(self, request, extra_context = {}) : 
         class PendingSheetForm(forms.Form) : 
