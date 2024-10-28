2ae6db6872a50179cbb1b1d7ddf1848d92f464f1
Venkatesh2304 <venkateshks2304@gmail.com>
2024-10-28 14:24:47 +0530

    s

--- app/admin.py
+++ app/admin.py
@@ -1420,7 +1420,8 @@ class SalesmanPendingSheetAdmin(CustomAdminModel) :
             for page in reader.pages:
                 writer.add_page(page)
             if len(reader.pages) % 2 != 0:
-                writer.add_blank_page() 
+                writer.add_blank_page()
+            bytesio.close()
 
         writer.write("pending_sheet.pdf")
         writer.close()
