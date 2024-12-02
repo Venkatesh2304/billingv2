151af5b15850e8f7bf52803c21474f7397d94001
Venkatesh2304 <96430022+Venkatesh2304@users.noreply.github.com>
2024-12-02 14:13:41 +0530

    s

--- app/admin.py
+++ app/admin.py
@@ -131,28 +131,28 @@ def create_simple_admin_list_filter(title_name,paramter,lookups: dict[str,Callab
                     return queryset_filter(queryset)
             return queryset
 
-        def choices(self, changelist):
-            add_facets = changelist.add_facets
-            facet_counts = self.get_facet_queryset(changelist) if add_facets else None
-            if allow_all : 
-                yield {
-                "selected": self.value() is None,
-                "query_string": changelist.get_query_string(remove=[self.parameter_name]),
-                "display": _("All"),
-                }
-            for i, (lookup, title) in enumerate(self.lookup_choices):
-                if add_facets:
-                    if (count := facet_counts.get(f"{i}__c", -1)) != -1:
-                        title = f"{title} ({count})"
-                    else:
-                        title = f"{title} (-)"
-                yield {
-                    "selected": self.value() == str(lookup),
-                    "query_string": changelist.get_query_string(
-                        {self.parameter_name: lookup}
-                    ),
-                    "display": title,
-                }
+        # def choices(self, changelist):
+        #     add_facets = changelist.add_facets
+        #     facet_counts = self.get_facet_queryset(changelist) if add_facets else None
+        #     if allow_all : 
+        #         yield {
+        #         "selected": self.value() is None,
+        #         "query_string": changelist.get_query_string(remove=[self.parameter_name]),
+        #         "display": _("All"),
+        #         }
+        #     for i, (lookup, title) in enumerate(self.lookup_choices):
+        #         if add_facets:
+        #             if (count := facet_counts.get(f"{i}__c", -1)) != -1:
+        #                 title = f"{title} ({count})"
+        #             else:
+        #                 title = f"{title} (-)"
+        #         yield {
+        #             "selected": self.value() == str(lookup),
+        #             "query_string": changelist.get_query_string(
+        #                 {self.parameter_name: lookup}
+        #             ),
+        #             "display": title,
+        #         }
                 
     return CustomListFilter   
 
