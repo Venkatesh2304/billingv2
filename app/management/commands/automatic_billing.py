6dde1237585b3b7f98d7ec74ac3a9a32baeed9b2
Venkatesh2304 <96430022+Venkatesh2304@users.noreply.github.com>
2024-12-05 23:42:41 +0530

    s

--- app/management/commands/automatic_billing.py
+++ app/management/commands/automatic_billing.py
@@ -13,6 +13,7 @@ class Command(BaseCommand):
     help = "Command to run automatic billing"
     def run_billing(self) : 
         billing_form = BillingForm({"date":datetime.date.today(),"max_lines":100})
+        billing_form.is_valid()
         billing_log = models.Billing(start_time = datetime.datetime.now(), status = 2,date = billing_form.cleaned_data.get("date"), automatic = True )
         billing_log.save()  
         for process_name in billing_process_names :
